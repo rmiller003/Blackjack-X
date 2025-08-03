848566dec65048b83d9a9722c2ed3571fc5cd254
Robert Miller <rmiller003@gmail.com>
2025-08-02 23:40:42 -0400

    Title Font

--- gui.py
+++ gui.py
@@ -18,7 +18,7 @@ class BlackjackGUI:
         self.background_image = tk.PhotoImage(file="background/casino.png")
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.background_image)
 
-        self.title_label = tk.Label(self.canvas, text="Welcome to Miller's Casino", font=("Arial", 40), fg="white", bg="darkgreen")
+        self.title_label = tk.Label(self.canvas, text="Welcome to Miller's Casino", font=("BaskervillerOld Face", 40), fg="white", bg="black")
         self.canvas.create_window(512, 50, window=self.title_label)
 
         self.blackjack_game = Blackjack()
