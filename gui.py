a4b1a499b7a3970b95654c610689320405965705
Robert Miller <rmiller003@gmail.com>
2025-08-04 00:02:57 -0400

    background image

--- gui.py
+++ gui.py
@@ -24,7 +24,6 @@ class BlackjackGUI:
         self.master.title("Blackjack")
         self.master.geometry("1024x768")
 
-        #
         pygame.mixer.init()
         pygame.mixer.music.load(resource_path("music/jazz.ogg"))
         pygame.mixer.music.play(loops=-1)
@@ -33,7 +32,7 @@ class BlackjackGUI:
 
         self.canvas = tk.Canvas(master, width=1024, height=768)
         self.canvas.pack()
-        #self.background_image = tk.PhotoImage(file="background/casino.png")
+        self.background_image = tk.PhotoImage(file="background/casino.png")
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.background_image)
 
         self.title_label = tk.Label(self.canvas, text="Welcome to Miller's Casino", font=("Edwardian Script ITC", 46), fg="white", bg="saddle brown")
